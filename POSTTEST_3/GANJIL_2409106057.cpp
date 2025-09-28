35346c725cddb21e5c2042e5679fb8589eab7afb
EuphoriaBot <dimasfirja80@gmail.com>
2025-09-28 19:34:27 +0800

    Finish Posttest 3

--- POSTTEST_3/GANJIL_2409106057.cpp
+++ POSTTEST_3/GANJIL_2409106057.cpp
@@ -12,6 +12,7 @@ struct Node
 {
     Inventory inv;
     Node *next;
+    Node *prev;
 };
 
 void addFirst(Node *&head, string NamaItem, int JumlahItem, string TipeItem)
@@ -20,7 +21,12 @@ void addFirst(Node *&head, string NamaItem, int JumlahItem, string TipeItem)
     nodeBaru->inv.NamaItem = NamaItem;
     nodeBaru->inv.JumlahItem = JumlahItem;
     nodeBaru->inv.TipeItem = TipeItem;
+    nodeBaru->prev = nullptr;
     nodeBaru->next = head;
+    if (head != nullptr)
+    {
+        head->prev = nodeBaru;
+    }
     head = nodeBaru;
 }
 
@@ -34,6 +40,7 @@ void addLast(Node *&head, string NamaItem, int JumlahItem, string TipeItem)
 
     if (head == nullptr)
     {
+        nodeBaru->prev = nullptr;
         head = nodeBaru;
         return;
     }
@@ -43,6 +50,7 @@ void addLast(Node *&head, string NamaItem, int JumlahItem, string TipeItem)
         temp = temp->next;
     }
     temp->next = nodeBaru;
+    nodeBaru->prev = temp;
 }
 
 void addSpecific(Node *&head, int Sisipan, string NamaItem, int JumlahItem, string TipeItem)
@@ -73,7 +81,14 @@ void addSpecific(Node *&head, int Sisipan, string NamaItem, int JumlahItem, stri
         nodeBaru->inv.NamaItem = NamaItem;
         nodeBaru->inv.JumlahItem = JumlahItem;
         nodeBaru->inv.TipeItem = TipeItem;
+
         nodeBaru->next = temp->next;
+        nodeBaru->prev = temp;
+
+        if (temp->next != nullptr)
+        {
+            temp->next->prev = nodeBaru;
+        }
         temp->next = nodeBaru;
     }
 }
@@ -92,12 +107,12 @@ void deleteLast(Node *&head)
         return;
     }
     Node *temp = head;
-    while (temp->next->next != nullptr)
+    while (temp->next != nullptr)
     {
         temp = temp->next;
     }
-    delete temp->next;
-    temp->next = nullptr;
+    temp->prev->next = nullptr;
+    delete temp;
 }
 
 void deleteSpesific(Node *&head, string nama)
@@ -109,11 +124,9 @@ void deleteSpesific(Node *&head, string nama)
     }
 
     Node *temp = head;
-    Node *prev = nullptr;
 
     while (temp != nullptr && temp->inv.NamaItem != nama)
     {
-        prev = temp;
         temp = temp->next;
     }
 
@@ -130,14 +143,20 @@ void deleteSpesific(Node *&head, string nama)
     if (temp->inv.JumlahItem <= 0)
     {
         cout << "Item " << nama << " Sudah Habis" << endl;
-        if (prev == nullptr)
+        if (temp->prev == nullptr)
         {
             head = temp->next;
+            if (head != nullptr)
+                head->prev = nullptr;
             delete temp;
         }
         else
         {
-            prev->next = temp->next;
+            temp->prev->next = temp->next;
+            if (temp->next != nullptr)
+            {
+                temp->next->prev = temp->prev;
+            }
             delete temp;
         }
     }
@@ -163,6 +182,50 @@ void showInventory(Node *head)
     }
 }
 
+void showInventoryReverse(Node *head)
+{
+    if (head == nullptr)
+    {
+        cout << "Inventory Kosong" << endl;
+        return;
+    }
+
+    Node *tail = head;
+    while (tail->next != nullptr)
+    {
+        tail = tail->next;
+    }
+
+    cout << "===== INVENTORY GAME =====" << endl;
+    while (tail != nullptr)
+    {
+        cout << "Nama   : " << tail->inv.NamaItem << endl;
+        cout << "Jumlah : " << tail->inv.JumlahItem << endl;
+        cout << "Tipe   : " << tail->inv.TipeItem << endl;
+        cout << "--------------------------" << endl;
+        tail = tail->prev;
+    }
+}
+
+void showDetail(Node *head, string nama)
+{
+    Node *temp = head;
+    while (temp != nullptr)
+    {
+        if (temp->inv.NamaItem == nama)
+        {
+            cout << "===== DETAIL ITEM =====" << endl;
+            cout << "Nama   : " << temp->inv.NamaItem << endl;
+            cout << "Jumlah : " << temp->inv.JumlahItem << endl;
+            cout << "Tipe   : " << temp->inv.TipeItem << endl;
+            cout << "=======================" << endl;
+            return;
+        }
+        temp = temp->next;
+    }
+    cout << "Item Dengan Nama " << nama << " Tidak Ditemukan" << endl;
+}
+
 int main()
 {
     Node *head = nullptr;
@@ -181,7 +244,9 @@ int main()
         cout << "2. Sisipkan Item                              " << endl;
         cout << "3. Hapus Item Terakhir                        " << endl;
         cout << "4. Gunakan Item                               " << endl;
-        cout << "5. Tampilkan Inventory                        " << endl;
+        cout << "5. Tampilkan Inventory Dari Depan             " << endl;
+        cout << "6. Tampilkan Inventory Dari Belakang          " << endl;
+        cout << "7. Tampilkan Detail Item                      " << endl;
         cout << "0. Keluar                                     " << endl;
         cout << "----------------------------------------------" << endl;
         cout << "Pilih menu: ";
@@ -216,6 +281,16 @@ int main()
         else if (pilihan == 5)
         {
             showInventory(head);
+        }
+        else if (pilihan == 6)
+        {
+            showInventoryReverse(head);
+        }
+        else if (pilihan == 7)
+        {
+            cout << "Masukkan Nama Item untuk Detail: ";
+            cin >> NamaItem;
+            showDetail(head, NamaItem);
         }
         else if (pilihan == 0)
         {
